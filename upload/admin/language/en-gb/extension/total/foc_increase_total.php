ebf02d109b7330438bcfc299ee8f9fed6599978f
Sergey Tominoff <ikenfin@gmail.com>
2020-06-17 03:12:08 +0400

    :speech_balloon: text_success added

--- upload/admin/language/en-gb/extension/total/foc_increase_total.php
+++ upload/admin/language/en-gb/extension/total/foc_increase_total.php
@@ -4,6 +4,7 @@ $_['heading_title'] = 'FOC: Increase order total';
 
 $_['text_edit'] = 'Configure';
 $_['text_extension'] = 'Extensions';
+$_['text_success'] = 'Settings successfuly saved';
 
 $_['entry_status'] = 'Status';
 $_['entry_sort_order'] = 'Sort order';
