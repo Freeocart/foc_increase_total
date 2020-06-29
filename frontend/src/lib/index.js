3876fa4acada15487b8e450ff0806074ae244440
Sergey Tominoff <ikenfin@gmail.com>
2020-06-29 12:16:13 +0400

    added mergeLeft fn

--- frontend/src/lib/index.js
+++ frontend/src/lib/index.js
@@ -2,3 +2,22 @@ export const intOrNull = value => {
   const number = parseInt(value)
   return isNaN(number) ? null : number
 }
+
+export const T = () => true
+
+export const mergeLeft = (a = {}, b = {}, checkFn = T) => {
+  const keys = Object.keys(a)
+  if (keys.length > 0) {
+    return keys.reduce((prev, key) => {
+      if (b.hasOwnProperty(key) && checkFn(b[key])) {
+        prev[key] = b[key]
+      }
+      else {
+        prev[key] = a[key]
+      }
+
+      return prev
+    }, {})
+  }
+  return a
+}
